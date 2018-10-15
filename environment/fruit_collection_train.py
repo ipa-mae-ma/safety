60a162f3d3867dedefedf671de48b80de28affa4
Marcel Albus <marcel.albus@ipa.fraunhofer.de>
2018-10-15 17:00:27 +0200

    [CODE] update main function

--- environment/fruit_collection_train.py
+++ environment/fruit_collection_train.py
@@ -5,11 +5,13 @@ Created on October 1, 2018
 @author: mae-ma
 @attention: fruit game for the safety DRL package using different architectures
 @contact: albus.marcel@gmail.com (Marcel Albus)
-@version: 1.1.0
+@version: 2.0.0
 
 #############################################################################################
 
 History:
+- v2.0.0: update main function for better structure
+- v1.1.1: save reward output in textfile
 - v1.1.0: dqn input fixed -> working, overblow input image
 - v1.0.2: first dqn test
 - v1.0.1: extend path to find other packages
@@ -21,6 +23,7 @@ from copy import deepcopy
 import pygame
 import numpy as np
 import time
+import yaml
 # or FruitCollectionLarge or FruitCollectionMini
 from fruit_collection import FruitCollection, FruitCollectionSmall, FruitCollectionLarge
 
@@ -48,9 +51,22 @@ RED = (255, 0, 0)
 BLUE = (0, 100, 255)
 WALL = (80, 80, 80)
 ############################
+np.set_printoptions(threshold=np.nan)
 
 
 class FruitCollectionTrain(FruitCollection):
+    def __init__(self):
+        self.env = FruitCollectionSmall(rendering=False, lives=1, is_fruit=True, is_ghost=True, image_saving=False)
+        # self.env.render()
+
+        # input_dim = (14, 21, 1) # (img_height, img_width, n_channels)
+        self.overblow_factor = 8
+        self.input_dim = (88, 88, 1)  # (img_height, img_width, n_channels)
+        self.mc = misc
+        self.dqn = DeepQNetwork(env=self.env, input_size=self.input_dim, output_size=4, name='DQN')
+        self.a3c = AsynchronousAdvantageActorCritic()
+        self.hra = HybridRewardArchitecture()
+
     def init_with_mode(self):
         self.is_ghost = False
         self.is_fruit = True
@@ -90,64 +106,56 @@ class FruitCollectionTrain(FruitCollection):
                 self.active_fruits[idx] = True
                 self.mini_target[self.possible_fruits.index(f)] = True
 
-    def main(self):
+    def main(self, verbose=False):
         reward = []
-        env = FruitCollectionSmall(rendering=True, lives=1, is_fruit=True, is_ghost=True, image_saving=False)
-        env.reset()
-        env.render()
+        for e in range(self.dqn.episode_max_len):
+            states = []
+            ep_reward = []
+            self.env.reset()
 
-        # input_dim = (14, 21, 1) # (img_height, img_width, n_channels)
-        input_dim = (88, 88, 1) # (img_height, img_width, n_channels)
-        mc = misc
-        dqn = DeepQNetwork(env=env, input_size=input_dim, output_size=4, name='DQN')
-        a3c = AsynchronousAdvantageActorCritic()
-        hra = HybridRewardArchitecture()
-        states = []
+            for t in range(500):
+                if t == 0:
+                    action = np.random.choice(self.env.legal_actions)
+                else:
+                    action = self.dqn.act(states[-1])
+                obs, r, terminated, info = self.env.step(action)
+                state_low = obs[2, ...]
+                # state_high = mc.overblow(input_array=state_low, factor=overblow_factor)
+                # state = state_high.reshape(input_dim)
+                # append grayscale image to state list
+                states.append(self.mc.make_frame(obs, do_overblow=True, overblow_factor=self.overblow_factor))
+                # states.append(state)
+                if t >= 1:
+                    state_t = states[-2]
+                    state_t1 = states[-1]
+                    self.dqn.remember(state=state_t, action=action, reward=r, next_state=state_t1, done=terminated)
+                # self.env.render()
 
-        for t in range(500):
-            action = np.random.choice(env.legal_actions)
-            obs, r, terminated, info = env.step(action)
-            state_low = obs[2, ...]
-            state_high = mc.overblow(input_array=state_low, factor=8)
-            state = state_high.reshape(input_dim)
-            states.append(state)
-            if t >= 1:
-                state_t = states[-2]
-                state_t1 = states[-1]
-                dqn.remember(state=state_t, action=action, reward=r, next_state=state_t1, done=terminated)
-            env.render()
-            if t == 50:
-                dqn.replay()
-                dqn.save_buffer(path='replay_buffer.pkl')
-                dqn.save_weights(path='weights.h5')
+                if verbose:
+                    print("\033[2J\033[H\033[2J", end="")
+                    print()
+                    print('pos: ', self.env.player_pos_x, self.env.player_pos_y)
+                    print('reward: ', r)
+                    print('state:')
+                    print(state_low)
+                    print('─' * 30)
+                    print('─' * 30)
 
-            print("\033[2J\033[H\033[2J", end="")
-            print()
-            print('pos: ', env.player_pos_x, env.player_pos_y)
-            print('reward: ', r)
-            print('state:')
-            print(state_low)
-            print('─' * 30)
-            frame = np.zeros(shape=obs[0, ...].shape, dtype=np.float32)
-            # wall
-            frame[obs[0, ...] != 0] = env.rgb2grayscale(WALL, normalization=False)
-            # fruit
-            frame[obs[1, ...] != 0] = env.rgb2grayscale(BLUE, normalization=False)
-            # pacman
-            frame[obs[2, ...] != 0] = env.rgb2grayscale(WHITE, normalization=False)
-            # ghosts
-            frame[obs[3, ...] != 0] = env.rgb2grayscale(RED, normalization=False)
-            print('─' * 30)
-
-            if terminated == False:
-                reward.append(r)
-            time.sleep(.01)
-            if terminated == True:
-                print(sum(reward))
-                reward = []
-                # self.main()
+                if terminated == False:
+                    reward.append(r)
+                    # time.sleep(.1)
+                if terminated == True:
+                    self.dqn.do_training(is_testing=False)
+                    self.dqn.save_buffer(path='replay_buffer.pkl')
+                    self.dqn.save_weights(path='weights.h5')
+                    print('episode: {}/{}, score: {}'.format(e, self.dqn.episode_max_len, sum(ep_reward)))
+                    reward.append(sum(ep_reward))
+                    ep_reward = []
+                    with open('reward.yml', 'w') as f:
+                        yaml.dump(reward, f)
+                    break
 
 
 if __name__ == '__main__':
     fct = FruitCollectionTrain()
-    fct.main()
+    fct.main(verbose=False)
