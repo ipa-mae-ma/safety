d770e1bbbc06df2dfd289f1471659cf529ec785e
Marcel Albus <marcel.albus@ipa.fraunhofer.de>
2018-10-16 12:55:09 +0200

    [CODE] update names

--- architectures/dqn.py
+++ architectures/dqn.py
@@ -45,12 +45,13 @@ from architectures.misc import Font
 # TODO: add update from target network instead of only one network
 # TODO: compare pyplot output of network after training with pyplot
 # TODO: implement warmstart
+# TODO: update _replay to use array batches instead of loop
 
 class DeepQNetwork:
     def __init__(self,
                  env,
-                 input_size: tuple=(1, 14, 21, 1),
-                 output_size: int=4,
+                 input_dim: tuple=(1, 14, 21, 1),
+                 output_dim: int=4,
                  name: str='DQN') -> None:
         """
         DQN Agent can:
@@ -72,8 +73,8 @@ class DeepQNetwork:
         self.rng = nprs(self.params['random_seed'])
 
         # self.mdp = mdp
-        self.input_size = input_size
-        self.output_size = output_size
+        self.input_dim = input_dim
+        self.output_dim = output_dim
         self.l_rate = self.params['learning_rate']
         self.minibatch_size = self.params['minibatch_size']
         self.gamma = self.params['gamma']
@@ -103,7 +104,7 @@ class DeepQNetwork:
         """
         model = keras.Sequential()
         # first hidden layer
-        model.add(keras.layers.Conv2D(input_shape=(88, 88, 1), batch_size=1, filters=32,
+        model.add(keras.layers.Conv2D(input_shape=self.input_dim, batch_size=1, filters=32,
                                       kernel_size=(8, 8), strides=4, activation='relu', data_format="channels_last"))
         # second hidden layer
         model.add(keras.layers.Conv2D(filters=64, kernel_size=(4, 4), strides=2, activation='relu'))
@@ -114,7 +115,7 @@ class DeepQNetwork:
         # fourth hidden layer
         model.add(keras.layers.Dense(512, activation='relu'))
         # output layer
-        model.add(keras.layers.Dense(self.output_size, activation='relu'))
+        model.add(keras.layers.Dense(self.output_dim, activation='relu'))
         # compile model
         model.compile(optimizer=tf.train.RMSPropOptimizer(learning_rate=self.l_rate,
                                                           decay=0.9,
@@ -126,7 +127,7 @@ class DeepQNetwork:
         target_model = keras.models.clone_model(model)
         return model, target_model
 
-    def do_training(self, total_eps=10, eps_per_epoch=100, eps_per_test=100, is_learning=True, is_testing=True):
+    def do_training(self, total_eps=20, eps_per_epoch=100, eps_per_test=100, is_learning=True, is_testing=True):
         """
         train DQN algorithm with replay buffer and minibatch
         """
@@ -152,13 +153,13 @@ class DeepQNetwork:
         """
         self.target_model.set_weights(self.model.get_weights())
 
-    def act(self, state: int) -> float:
+    def act(self, state) -> int:
         """
         return action from neural net
         Input:
             state (np.array): the current state as shape (1, img_height, img_width, 1)
         Output:
-            action (float): probability to choose action
+            action (int): action number
         """
         state = state[np.newaxis, ...]
         q_vals = self.model.predict(state)
