32091875bf0bae97a93ca4653b9152120fa96d77
Marcel Albus <marcel.albus@ipa.fraunhofer.de>
2018-10-22 10:11:14 +0200

    [CODE] use click for terminal commands

--- architectures/evaluation.py
+++ architectures/evaluation.py
@@ -4,20 +4,23 @@ Created on October 19, 2018
 @author: mae-ma
 @attention: evaluation of the architectures
 @contact: albus.marcel@gmail.com (Marcel Albus)
-@version: 1.0.0
+@version: 1.1.0
 
 #############################################################################################
 
 History:
+- v1.1.0: add click commands
 - v1.0.0: first init
 """
+
 import numpy as np
 from matplotlib import pyplot as plt
 import pickle
 import yaml
 import shutil
 import os
-
+import click
+import datetime
 
 def smooth(x, window_len=11, window='hanning'):
     """smooth the data using a window with requested size.
@@ -77,19 +80,22 @@ def smooth(x, window_len=11, window='hanning'):
 
 class Evaluation:
     def __init__(self):
+        # src_filepath = home/mae-ma/git/safety
+        self.src_filepath = os.path.dirname(os.path.dirname(__file__))
+        
         with open('/home/mae-ma/git/safety/reward.yml', 'r') as file:
             self.yml = yaml.load(file)
 
-        self.csv = np.genfromtxt(
-            '/home/mae-ma/git/safety/training_log_DQN.csv', delimiter=',')
-
-        self.src_filepath = os.path.dirname(os.path.dirname(__file__))
         self.tgt_filepath = os.path.join(os.path.dirname(
             os.path.dirname(__file__)), 'results')
         if not os.path.exists(self.tgt_filepath):
             os.makedirs(self.tgt_filepath)
 
     def plot(self):
+        print('–'*50)
+        print('Plot "reward.yaml"')
+        self.csv = np.genfromtxt(
+            '/home/mae-ma/git/safety/training_log_DQN.csv', delimiter=',')
         smoothed = smooth(self.csv[:, 2], 31)
         fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 12))
         ax1.plot([_ for _ in range(len(smoothed))],
@@ -110,20 +116,36 @@ class Evaluation:
         fig.tight_layout()
         plt.savefig('loss.pdf')
         plt.show()
+        print('–'*50)
 
     def save_all(self):
+        print('–'*50)
         filelist = ['weights.h5', 'target_weights.h5',
                     'reward.yml', 'replay_buffer.pkl', 'training_log_DQN.csv',
-                    'loss.pdf', 'architectures/config_dqn.yaml']
-        folder = 'test'
+                    'loss.pdf', 'architectures/config_dqn.yaml', 'model.yml']
+        folder = datetime.datetime.today().strftime('%Y_%m_%d-%H_%M')
         folderpath = os.path.join(self.tgt_filepath, folder)
+        print('Save all files to: ' + folderpath)
         if not os.path.exists(folderpath):
             os.makedirs(folderpath)
         for file in filelist:
             shutil.copy2(os.path.join(self.src_filepath, file), folderpath)
+        print('–'*50)
+
+
+@click.command()
+@click.option('--plot/-no-plot', '-p/-np', default=True, help='plot the results from the "results.yaml" file')
+@click.option('--save/--no-save', '-s/-ns', default=True, help='backups the files')
+def main(plot, save):
+    ev = Evaluation()
+    if save:
+        ev.save_all()
+
+    if plot:
+        ev.plot()
+
+
 
 
 if __name__ == '__main__':
-    ev = Evaluation()
-    ev.plot()
-    # ev.save_all()
+    main()
